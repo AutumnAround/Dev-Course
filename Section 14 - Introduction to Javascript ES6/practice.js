9f9479ddeac7657cb83790ce91f958a2ce50af81
AutumnAround <67914593+AutumnAround@users.noreply.github.com>
2024-06-06 15:56:39 +0100

    Add files via upload

---
+++ Section 14 - Introduction to Javascript ES6/practice.js
@@ -0,0 +1,119 @@
+// var tweet = prompt("Compose your tweet:");
+// alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.");
+
+//________________________________________________________________________________________________
+
+// var tweet = prompt("Compose your tweet:");
+// var tweetUnder140 = tweet.slice(0,140);
+// alert(tweetUnder140);
+//________________________________________________________________________________________________
+
+// var userName = prompt("Say your name")
+// var userNameFirstLetter = userName.slice(0,1); 
+// var userNameFirstLetter = userNameFirstLetter.toUpperCase();
+// var userNameOtherLetters = userName.slice(1,userName.length);
+// var userNameOtherLetters = userNameOtherLetters.toLowerCase();
+// alert("Hello " + userNameFirstLetter + userNameOtherLetters + " There");
+
+//________________________________________________________________________________________________
+
+
+// var yourDogHumanAge = prompt("Conventer dog age to human age - insert your Doggy`s age")
+// var humanAge = (yourDogHumanAge -2) * 4 + 21;
+// alert("Your doggy`s age as a human" + "is " + humanAge);
+
+//________________________________________________________________________________________________
+
+// function getMilk(money) {   
+//   console.log("leaveHouse");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   var numberOfBottles = Math.floor(money  / 1.5);
+//   console.log("buy "  + numberOfBottles + " bottles of milk");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("enterHouse");
+//   console.log("moneyLeft");
+// }
+// getMilk(12)
+
+//________________________________________________________________________________________________
+
+// var daysLeft; // x 365
+//     var weeksLeft; // y 52
+//     var monthsLeft; // z 12    
+//     daysLeft= (90 - age) * 365;
+//     weeksLeft= (90 - age) * 52;
+//     monthsLeft= (90 - age) * 12;   
+//     console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+
+//________________________________________________________________________________________________
+
+// function getMilk(money) {   
+//   console.log("leaveHouse");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   var numberOfBottles = Math.floor(money  / 1.5);
+//   console.log("buy "  + numberOfBottles + " bottles of milk");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("enterHouse");
+//   console.log("moneyLeft");
+//   return money % 1.5;
+// }
+//  var change = getMilk(11)
+// console.log("change is " + change);
+
+//________________________________________________________________________________________________
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+/* 
+function bmiCalculator(weight,height) {
+    var weight = weight;
+    var height = height;
+    var bmi = weight / (height*height);
+    bmiCalculator = Math.floor(bmi);
+}
+
+bmiCalculator(65,1.8);
+console.log(bmiCalculator);
+ */
+
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+
+//________________________________________________________________________________________________
